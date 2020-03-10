692b2f2969b8527702517a3e2d61c1195368d40c
Bastien P <bastien.pinard@ynov.com>
2020-03-10 21:25:37 +0100

    fix: title right form

--- src/containers/right-form/index.js
+++ src/containers/right-form/index.js
@@ -12,29 +12,48 @@ class RightForm extends Component {
     console.log(this.props.data.fieldFocus);
 
     if (this.props.data.fieldFocus === "departCity") {
-      displayCities = this.props.data.suggestCities.map((el, index) => {
-        return (
-          <div>
-            {/* <h3>Quel est votre trajet ?</h3> */}
-            <div key={index} onClick={() => this.props.choosenDepartCity(el)}>
-              {el}
-            </div>
-          </div>
-        );
-      });
+      return (
+        <div className="right-form">
+          <h3>Quel est votre trajet ?</h3>
+
+          {
+            (displayCities = this.props.data.suggestCities.map((el, index) => {
+              return (
+                <div>
+                  <div
+                    key={index}
+                    onClick={() => this.props.choosenDepartCity(el)}
+                  >
+                    {el}
+                  </div>
+                </div>
+              );
+            }))
+          }
+        </div>
+      );
     } else if (this.props.data.fieldFocus === "arrivalCity") {
-      displayCities = this.props.data.suggestCities.map((el, index) => {
-        return (
-          <div>
-            {/* <h3>Choisissez une gare d’arrivée</h3> */}
-            <div key={index} onClick={() => this.props.choosenArrivalCity(el)}>
-              {el}
-            </div>
-          </div>
-        );
-      });
+      return (
+        <div className="right-form">
+          <h3>Choisissez une gare d’arrivée</h3>
+          {
+            (displayCities = this.props.data.suggestCities.map((el, index) => {
+              return (
+                <div>
+                  <div
+                    key={index}
+                    onClick={() => this.props.choosenArrivalCity(el)}
+                  >
+                    {el}
+                  </div>
+                </div>
+              );
+            }))
+          }
+        </div>
+      );
     }
-    return <div className="right-form">{displayCities}</div>;
+    return <div>{displayCities}</div>;
   }
 }
 
